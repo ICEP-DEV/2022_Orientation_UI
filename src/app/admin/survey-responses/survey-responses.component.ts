9d60bd09cac9d3ab25c37a42ad8619c6bc353b5f
CebolenkosiShezi <cbshezi5@gmail.com>
2022-04-20 08:06:25 +0200

    logoutadmin

--- src/app/admin/survey-responses/survey-responses.component.ts
+++ src/app/admin/survey-responses/survey-responses.component.ts
@@ -45,7 +45,7 @@ export class SurveyResponsesComponent implements OnInit  {
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.filter = filterValue.toString();
   
 
     if (this.dataSource.paginator) {
