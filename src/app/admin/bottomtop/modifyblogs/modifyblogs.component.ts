8d2236c2e7bcb10de4dfe88ecef06443d7eb57c9
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-05-21 17:52:13 +0200

    Update modifyblogs.component.ts

--- src/app/admin/bottomtop/modifyblogs/modifyblogs.component.ts
+++ src/app/admin/bottomtop/modifyblogs/modifyblogs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from './../../../blog/blog.component'
+import { UserService } from './../../../user.service';
 
 @Component({
   selector: 'app-modifyblogs',
@@ -8,11 +9,16 @@ import { Blog } from './../../../blog/blog.component'
 })
 export class ModifyblogsComponent implements OnInit {
 
-  dataSouce : Blog = {title:"",description:"",author:"",created_on:"",path:"",link:"",subTittle:""}
+  blogData : Blog[] =  [{author:"",created_on:"",description:"",link:"",path:"",subTittle:"",title:""}]
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    private _userService : UserService
+  ) {
+    this._userService.getAllBlogs().subscribe((result)=>{
+      if(result.error) throw result.message
+      else
+        this.blogData = result.data
+    })
+   }
 
 }
