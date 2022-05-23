0f9c157e8671faeca02d2f33342527618a48d3e8
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-05-24 00:25:56 +0200

    https

--- src/globals.ts
+++ src/globals.ts
@@ -6,7 +6,7 @@ export const GRAPH_HOSTNAME = "https://frxu82wy28.us-east-1.awsapprunner.com"
 export const APP_HOSTNAME = "http://ec2-18-234-218-224.compute-1.amazonaws.com"
 
 //SOCKETIO ENDPOINT
-export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
+export const SOCK_HOSTNAME = "https://ec2-54-234-88-147.compute-1.amazonaws.com"
 
 //Apache FTP (SMTP,PDFRender,Frontend) 
 export const APACHE_HOST = "http://ec2-54-234-70-68.compute-1.amazonaws.com";
