2ff6f4ddf7f52e2cb1fed17c60e0d7c5e3f21909
CebolenkosiShezi <cbshezi5@gmail.com>
2022-05-24 10:44:27 +0200

    back to vm

--- src/globals.ts
+++ src/globals.ts
@@ -1,12 +1,12 @@
 
 //REST API ENDPOINT
-export const GRAPH_HOSTNAME = "https://frxu82wy28.us-east-1.awsapprunner.com"
+export const GRAPH_HOSTNAME = "http://ec2-34-235-114-194.compute-1.amazonaws.com"
 
 //FILE TRANSFER ENDPOINT
-export const APP_HOSTNAME = "https://qkdefufg7m.us-east-1.awsapprunner.com"
+export const APP_HOSTNAME = "http://ec2-18-234-218-224.compute-1.amazonaws.com"
 
 //SOCKETIO ENDPOINT
-export const SOCK_HOSTNAME = "https://ec2-54-234-88-147.compute-1.amazonaws.com"
+export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
 
 //Apache FTP (SMTP,PDFRender,Frontend) 
 export const APACHE_HOST = "https://ec2-54-234-70-68.compute-1.amazonaws.com";
